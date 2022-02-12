f0bca569496f6b6aad058c8fe3841eb58f856a16
Douglas Dutra Santana <douglasdsantana@gmail.com>
2022-02-12 00:05:41 -0300

    update

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50.5
+console.log(`A Velocidade do seu carro é ${vel}Km/h`)
+if (vel >60) { // Condição simples!
+    console.log('Você utrapassou a velocida permetida. MULTADO!!!')
+}
+console.log('Dirija sempre com o cinto de segurança!!!')
